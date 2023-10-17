c0a06b7bd3dd1a7a85dde2dfdf04da4d40bbb0a2
adi-pfc <adele.kosman@getpfc.com>
2023-10-17 22:31:16 +0200

    fix overlapping text in mobile

--- src/pages/Discography.js
+++ src/pages/Discography.js
@@ -10,13 +10,13 @@ function Discography() {
           <p class="header" style={{margin: '10px'}}>08.05.2018</p>
           <iframe title='1' style={{border: 0, width: '100%', height: '120px', boxShadow: '0px 0px 10px black'}} src="https://bandcamp.com/EmbeddedPlayer/album=277515953/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/artwork=small/transparent=true/" seamless><a href="https://amkosman.bandcamp.com/album/volvm-1-turbulence">VOLVM 1.turbulence by Adele Marcia Kosman</a></iframe>
         </div>
-        <div style={{marginLeft: '30px', top: '30px'}}>
-          <p class="text-header" style={{top: '30px', lineHeight: 2}}>Comfort</p>
+        <div style={{marginLeft: '30px', top: '30px', lineHeight: 2}}>
+          <p class="text-header" style={{top: '30px'}}>Comfort</p>
           <p class="header" style={{margin: '10px'}}>13.02.2017</p>
           <iframe title='2' style={{border: 0, width: '100%', height: '120px', boxShadow: '0px 0px 10px black'}} src="https://bandcamp.com/EmbeddedPlayer/track=103385570/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/artwork=small/transparent=true/" seamless><a href="https://amkosman.bandcamp.com/track/comfort">comfort by Adele Marcia Kosman</a></iframe>
           </div>
-          <div style={{marginLeft: '30px', top: '30px', whiteSpace: 'normal'}}>
-          <p class="text-header" style={{top: '30px', lineHeight: 2}}>No One Notice</p>
+          <div style={{marginLeft: '30px', top: '30px', lineHeight: 2}}>
+          <p class="text-header" style={{top: '30px'}}>No One Notice</p>
           <p class="header" style={{margin: '10px'}}>14.03.2016</p>
           <iframe title='3' style={{border: 0, width: '100%', height: '120px', boxShadow: '0px 0px 10px black'}} src="https://bandcamp.com/EmbeddedPlayer/track=3128159752/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/artwork=small/transparent=true/" seamless><a href="https://amkosman.bandcamp.com/track/no-one-notice">No One Notice by Adele Marcia Kosman</a></iframe>
         </div>
